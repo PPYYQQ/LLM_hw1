22e0052f9c3112b5af7f1176998145d37fd4544e
PPYYQQ <945505411@qq.com>
2024-12-23 03:15:36 +0800

    2100017731 GPT2 weigth sharing\

--- hw1-code/pretrain.py
+++ hw1-code/pretrain.py
@@ -96,6 +96,9 @@ class GPT(nn.Module):
         ))
         # Final classifier
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
+
+        # weight sharing
+        self.transformer.wte.weight = self.lm_head.weight
         
     def forward(self, idx, targets=None):
         B, T = idx.size()
