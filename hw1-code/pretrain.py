158e81d32cf960741b73d627836990fc62406ff4
PPYYQQ <945505411@qq.com>
2024-12-23 03:03:27 +0800

    2100017731 GPT2 dataloader

--- hw1-code/pretrain.py
+++ hw1-code/pretrain.py
@@ -171,6 +171,36 @@ class GPT(nn.Module):
         return model
 
 
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        enc = tiktoken.get_encoding('gpt2')
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        f.close()
+        self.tokens = torch.tensor(enc.encode(text))
+        print(f"Loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buffer = self.tokens[self.current : self.current + B*T+1]
+        x = (buffer[:-1]).view(B, T)
+        y = (buffer[1:]).view(B, T)
+
+        self.current += B * T
+
+        if self.current + (B*T+1) > len(self.tokens):
+            self.current = 0
+        
+        return x, y
+
 device = 'cpu'
 if torch.cuda.is_available():
     device = 'cuda'
@@ -223,30 +253,32 @@ print(f"Using model {device}")
 #     print(decoded)
 
 
+# Single batch
+# import tiktoken
+# enc = tiktoken.get_encoding('gpt2')
+# with open('input.txt', 'r') as f:
+#     text = f.read()
+# f.close()
+# text = text[:1000]
+# tokens = enc.encode(text)
+# B, T = 4, 32
+# buffer = torch.tensor(tokens[:B*T + 1]).to(device)
+# x = buffer[:-1].view(B, T)
+# y = buffer[1:].view(B, T)
 
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
-f.close()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buffer = torch.tensor(tokens[:B*T + 1]).to(device)
-x = buffer[:-1].view(B, T)
-y = buffer[1:].view(B, T)
+# model = GPT(GPTConfig())
+# model.to(device)
+# # logits, loss = model(x, y)
 
-model = GPT(GPTConfig())
-model.to(device)
-# logits, loss = model(x, y)
-
-optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
-for i in range(50):
-    optimizer.zero_grad()
-    logits, loss = model(x, y)
-    loss.backward()
-    optimizer.step()
-    print(f"Step {i}, loss: {loss.item()}")
+# optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
+# for i in range(50):
+#     optimizer.zero_grad()
+#     logits, loss = model(x, y)
+#     loss.backward()
+#     optimizer.step()
+#     print(f"Step {i}, loss: {loss.item()}")
 
 
-print(loss)
+# print(loss)
+
+
