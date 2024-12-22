642e4ebc81e3adcb7711a724f21876ffa1e3960b
PPYYQQ <945505411@qq.com>
2024-12-23 04:11:08 +0800

    2100017731 GPT2 flash attention 140ms

--- hw1-code/pretrain.py
+++ hw1-code/pretrain.py
@@ -30,12 +30,17 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, number of heads, seq length, head size)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2) # (B, number of heads, seq length, head size)
 
-        # attention
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        # only attend to the previous
-        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
-        att = F.softmax(att, dim = -1)
-        y = att @ v
+
+
+        # # attention
+        # att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # # only attend to the previous
+        # att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        # att = F.softmax(att, dim = -1)
+        # y = att @ v
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)# Flash attention
+
+
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         y = self.c_proj(y)
         return y
