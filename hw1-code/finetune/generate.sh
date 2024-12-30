73f8ea921a35af06f0d99b55ced160ba64560c3d
PPYYQQ <945505411@qq.com>
2024-12-30 13:08:16 +0000

    lora eval

--- hw1-code/finetune/generate.sh
+++ hw1-code/finetune/generate.sh
@@ -1,12 +1,12 @@
 python generate.py \
-    --model_name_or_path gpt2 \
+    --model_name_or_path /home/pku0008/.cache/huggingface/hub/models--gpt2/snapshots/607a30d783dfa663caf39e06633721c8d4cfcd7e \
     --max_length 512 \
     --trust_remote_code True \
-    --use_lora False \
-    --lora_dim 8 \
+    --use_lora True \
+    --lora_dim 32 \
     --lora_scaling 32 \
     --lora_module_name h. \
-    --lora_load_path path-to-lora-checkpoint \
+    --lora_load_path /home/pku0008/homework_1/LLM_hw1/hw1-code/finetune/results/gpt2-alpaca-lora-rank-32-20241225-044441/lora.pt \
     --seed 42 \
-    --use_cuda False \
-    --output_dir_name gpt2-alpaca-eval
+    --use_cuda True \
+    --output_dir_name gpt2-alpaca-lora-32-eval
